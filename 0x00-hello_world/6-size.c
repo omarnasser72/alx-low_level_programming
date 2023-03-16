25b40183d99154ef39c11130f0578215808673fd
omarnasser72 <omarnasser7272@gmail.com>
2023-03-17 01:33:03 +0200

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(a));
 	printf("Size of an int: %zu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
