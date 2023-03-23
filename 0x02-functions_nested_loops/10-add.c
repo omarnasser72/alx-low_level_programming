2507d77c382176b33ed4ae7f018223cd994e7345
omarnasser72 <omarnasser7272@gmail.com>
2023-03-23 04:45:40 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for char
+ * @c: parameter
+ *
+ * Return: on success 1.
+ *      on fail -1.
+*/
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
