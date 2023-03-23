5901b98dd6cb58f9ca50053b842cb17d5b187d78
omarnasser72 <omarnasser7272@gmail.com>
2023-03-23 16:58:03 -0700

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - display sq with size n
+ * @size: sq size length
+*/
+
+void print_square(int size)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
