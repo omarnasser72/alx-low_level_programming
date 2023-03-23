85c1e5a72603e1964fb8e47debc1802e7762076b
omarnasser72 <omarnasser7272@gmail.com>
2023-03-23 16:50:07 -0700

    7

--- 0x04-more_functions_nested_loops/t.c
+++ 0x04-more_functions_nested_loops/t.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints diagonal
- * @n: param
-*/
-
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
+ */
 void print_diagonal(int n)
 {
 	if (n <= 0)
