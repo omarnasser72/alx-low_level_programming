524d3382f096a40fa1c04f31841e4941eced7d14
omarnasser72 <omarnasser7272@gmail.com>
2023-03-23 18:30:23 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,16 +14,17 @@ void print_triangle(int size)
 	{
 		int i, j;
 
- 	for (i = 1; i <= size; i++)
-	{
-		for (j = i; j < size; j++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar(' ');
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		for (j = 1; j <= i; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
 	}
 }
