8b9f15263c8c6443da642df927fc93c0d5e0a24a
omarnasser72 <omarnasser7272@gmail.com>
2023-03-27 15:30:28 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - check for char
+ * @s: parameter
+ *
+*/
+
+void rev_string(char *s)
+{
+	int length, i;
+
+	length = _strlen(*s);
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
