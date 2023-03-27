83a0a6089ed38d917a8ac26df7d705ecfd494341
omarnasser72 <omarnasser7272@gmail.com>
2023-03-27 15:16:13 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - check for char
+ * @s: parameter
+ *
+ */
+
+void print_rev(char *s)
+{
+	int length, i;
+
+	length = _strlen(*s);
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
