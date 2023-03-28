1ac330f9c213187243208414c5de383273c7b891
omarnasser72 <omarnasser7272@gmail.com>
2023-03-27 18:15:08 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcpy - check for char
+ * @dest: parameter
+ * @src: parameter
+ *
+ * Return: on success 1.
+ *
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int len = 0;
+
+	while (*src != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < l; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
