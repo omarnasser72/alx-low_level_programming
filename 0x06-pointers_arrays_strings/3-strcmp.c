960675aaa71028c4cd2f5b37e44e3b9dacbad4e3
omarnasser72 <omarnasser7272@gmail.com>
2023-03-30 16:17:44 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - check for char
+ * @s1: parameter
+ * @s2: parameter
+ *
+ * Return: diff
+ *
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
