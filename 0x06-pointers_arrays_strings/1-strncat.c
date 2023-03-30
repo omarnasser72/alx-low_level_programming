806b707a37555855c42d20ed7a458e431cec48ad
omarnasser72 <105700689+omarnasser72@users.noreply.github.com>
2023-03-30 23:46:50 +0200

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,12 +22,11 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 
-	while (src[j] != '\0' && n == 0)
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
-		n--;
 	}
 	dest[i] = '\0';
 	return (dest);
