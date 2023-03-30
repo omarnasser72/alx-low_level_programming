e2c9dde22119eef358c536ce2c43cd47c8b3fcb8
omarnasser72 <omarnasser7272@gmail.com>
2023-03-30 16:32:15 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - check for char
+ * @a: parameter
+ * @n: size
+ *
+ *
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, t;
+
+	n--;
+	for (i = 0; i < n; i++, n--)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = a[i];
+	}
+}
