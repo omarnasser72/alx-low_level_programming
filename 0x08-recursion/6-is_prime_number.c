85349948a980787cc111f5b6fac4632e465eef97
omarnasser72 <105700689+omarnasser72@users.noreply.github.com>
2023-04-06 03:50:30 +0200

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,9 +10,9 @@ int actual_prime(int n, int i);
  */
 int is_prime_number(int n)
 {
-  if (n <= 1)
-    return (0);
-  return (actual_prime(n, n - 1));
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
 }
 
 /**
