adb232abbede387fc7215a51d0369d92901668d8
omarnasser72 <omarnasser7272@gmail.com>
2023-04-10 17:43:04 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- *  - check for char
- * @: parameter
+ * main - check for char
+ * @argc: parameter
+ * @argv: parameter
  *
- * Return: on success 1.
+ * Return: 0
  *
 */
 
 int main (int argc, char ** argv)
 {
+	void(argc);
 	printf("%s",argv[0]);
+	return (0);
 }
