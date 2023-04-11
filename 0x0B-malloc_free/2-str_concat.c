ea6f5bad8b5eba813ad2d7899c8dd51de791ae8d
omarnasser72 <omarnasser7272@gmail.com>
2023-04-11 16:15:38 +0200

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * str_concat - check for char
+ * @s1: parameter
+ * @s2: parameter
+ * Return: on success 1.
+ *
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int size1 = 0, size2 = 0;
+	int  i, j;
+	char *p;
+	for (i = 0; s1[i] != '\0'; i++)
+	;
+
+	for (i = 0; s2[i] != '\0'; i++)
+        ;
+	
+	p = (char *)malloc((size1 + size2) * sizeof(*s1) * sizeof(*s2) + 1);
+
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < size1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (j = 0; j < size2; j++,i++)
+	{
+		ptr[i] = s2[j];
+	}
+}
