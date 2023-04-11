814cf39b81190ed56053a6e0457f7182c6e624f1
omarnasser72 <omarnasser7272@gmail.com>
2023-04-10 18:27:01 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main - check for char
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0
+ *
+*/
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+        printf("%d\n",argc - 1);
+        return (0);
+}
