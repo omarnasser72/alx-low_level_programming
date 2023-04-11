17dab0c4951d2d019b8e4098c6c0167a79d8171c
omarnasser72 <omarnasser7272@gmail.com>
2023-04-11 16:54:19 +0200

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * alloc_grid - check for char
+ * @width: parameter
+ * @height: parameter
+ * Return: on success 1.
+ *
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int r, c;
+	int **arr;
+
+	arr = malloc(sizeof(*arr) * height);
+
+	if (width <= 0 || height <= 0 || arr == 0)
+		return (NULL);
+
+	for (r = 0; r < height; r++)
+	{
+		arr [r] = malloc(sizeof(**arr) * width);
+		if (arr[r] == 0)
+		{
+			while(r--)
+				free(arr[r]);
+			free(arr);
+			return (NULL);
+		}
+		for(c = 0; c < width; c++)
+			arr[r][c] = 0;
+	}
+	return (arr);
+
+}
