923cc17455ff1e3dc74b80e8c9c83f67d49ffc9c
omarnasser72 <omarnasser7272@gmail.com>
2023-04-11 15:06:15 +0200

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_array - check for char
+ * @size: parameter
+ * @c: paramter
+ * Return: on success 1.
+ *
+*/
+
+char *create_array(unsigned int size, char c);
+{
+	char* ptr = (char*)calloc(size, (size of char));
+	
+	if (ptr == NULL || size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
+		return (ptr);
+	}
+}
