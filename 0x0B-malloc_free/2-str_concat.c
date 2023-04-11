429536731eb015e755f64fceb7fd8a26ed987816
omarnasser72 <omarnasser7272@gmail.com>
2023-04-11 16:24:20 +0200

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,13 +14,18 @@ char *str_concat(char *s1, char *s2)
 	int  i, j;
 	char *p;
 	
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
+
 	for (i = 0; s1[i] != '\0'; i++)
 	;
 
 	for (i = 0; s2[i] != '\0'; i++)
 	;
 
-	p = (char *)malloc((size1 + size2) * sizeof(*s1) * sizeof(*s2) + 1);
+	p = (char *)malloc((size1 + size2) * sizeof(char) + 1);
 
 	if (p == NULL)
 		return (NULL);
@@ -32,5 +37,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		p[i] = s2[j];
 	}
+	p[i] = '\0';
 	return (p);
 }
