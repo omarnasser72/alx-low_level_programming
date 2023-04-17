302b24f03d65cbd8d67f2c49f105a570f368e69a
omarnasser72 <105700689+omarnasser72@users.noreply.github.com>
2023-04-18 01:43:55 +0200

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check for char
+ * @argc: parameter
+ * @argv: parameter
+ *
+ * Return: 0
+ *
+*/
+
+int main()
+{
+  _putchar("%s", __FILE__);
+  return(0);
+}
