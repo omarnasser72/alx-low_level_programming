1d56415e89745c724a130cb4ed91ada22e49dc23
omarnasser72 <omarnasser7272@gmail.com>
2023-04-18 01:59:22 +0000

    0x0E

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * print_dog - check for char
+ * @d: parameter
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (!d)
+	{
+
+	}
+	else
+	{
+		if (!(d->name))
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		if (!(d->age))
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %d\n", d->age);
+		if (!(d->owner))
+			printf("Owner: (nil)");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
