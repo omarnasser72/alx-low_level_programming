d0ef4f30bf258334acdcba1e963925186d221733
omarnasser72 <omarnasser7272@gmail.com>
2023-04-19 17:55:55 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,25 +12,19 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0, match = 0;
+	int i = 0;
 
-	if (array && cmp)
+	if (array && size && cmp)
 	{
 		for (; i < size; i++)
 		{
 			if (cmp(array[i]))
 			{
-				match = 1;
 				return (i);
 			}
 		}
-		if (match == 0)
-		{
-			return (-1);
-		}
+}
 	}
-	if (size <= 0)
-	{
-		return (-1);
-	}
+	return (-1);
+
 }
