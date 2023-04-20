1262ed60c096f14ca420ba032cfbf03d8570692a
omarnasser72 <omarnasser7272@gmail.com>
2023-04-19 17:32:33 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - check for char
+ * @name: parameter
+ * @f:fun
+ *
+ * Return: on success 1.
+ *
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef function_pointers_
+#define function_pointers_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif /*function_pointers*/
