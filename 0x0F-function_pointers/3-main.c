5d412c13d2735d553af2832c6912ac6d1b512efe
omarnasser72 <omarnasser7272@gmail.com>
2023-04-19 18:58:02 -0700

    3-get_op_func.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+
+/**
+ * main - main fun
+ * @argc: parameter count
+ * @argv: parameters
+ * Return: on success 1.
+ *
+*/
+
+int main(char argc, char **argv)
+{
+	int (*op_fun)(int, int), a, b;
+
+	if (argc != 4)
+		printf("Error"), exit(98);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_fun = get_op_func(argv[2][0]);
+
+	if (!op_fun)
+		printf("Error"), exit(99);
+
+	if (b == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error"), exit(100);
+	printf("%d\n", op_fun(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add
+ * @a: parameter
+ * @b: param
+ * Return: on success 1.
+ *
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub
+ * @a: parameter
+ * @b: param
+ * Return: on success 1.
+ *
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - add
+ * @a: parameter
+ * @b: param
+ * Return: on success 1.
+ *
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div
+ * @a: parameter
+ * @b: param
+ * Return: on success 1.
+ *
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - add
+ * @a: parameter
+ * @b: param
+ * Return: on success 1.
+ *
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
