48dabe0b85f33d3588f187058ff3f9f072b08331
omarnasser72 <omarnasser7272@gmail.com>
2023-04-30 09:11:40 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_listint - print no of nodes
+ * @h: ptr to listint_t
+ *
+ * Return: no of nodes
+ *
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{	
+		printf("%d\n", h->n);
+		h = h->next;
+		size++;
+	}
+	return (size);
+}
