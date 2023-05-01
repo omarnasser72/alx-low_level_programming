77d854b9bef4dc60fe77e61cdeeb6e87dcfc54a4
omarnasser72 <omarnasser7272@gmail.com>
2023-05-01 15:09:38 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (i < index - 1)
 	{
-		if (t == NULL || (t->next == NULL))
+		if (t == NULL || (t->next) == NULL)
 		{
 			return (-1);
 		}
