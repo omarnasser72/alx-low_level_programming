9e0a62a70c39598f70e4e7d9c1303b750e6737a0
omarnasser72 <omarnasser7272@gmail.com>
2023-05-01 16:44:44 -0700

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse list
+ * @head: head ptr to listint_t
+ * Return: return head
+*/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *n = NULL;
+	listint_t *p = NULL;
+
+	while (*head)
+	{
+		n = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = n;
+	}
+	*head = prev;
+	return (*head);
+}
