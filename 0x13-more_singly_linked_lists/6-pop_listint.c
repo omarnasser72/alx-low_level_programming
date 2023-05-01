9f23ecefced961c0ee3c9e83c9ac7c676983cc0b
omarnasser72 <omarnasser7272@gmail.com>
2023-05-01 08:18:39 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pop first element listint_t list
+ * @head: head ptr to listint_t
+ *
+ * Return: first element
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *t;
+	int n;
+
+	if (*head == NULL || head == NULL)
+		return (0);
+
+	n = *head->n;
+	t = (*head)->next;
+	free(*head);
+	*head = t;
+	return (n);
+}
