db9b4fcc7d04cf9eca6b00d9c92c32e747ac2232
omarnasser72 <105700689+omarnasser72@users.noreply.github.com>
2023-07-11 09:25:15 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,38 +1,34 @@
 #include "main.h"
-/**
- * _strlen - counts string length
- * @str: string to be counted
- *
- * Return: string length
-*/
-int _strlen(char *str)
-{
-	int i = 0;
 
-	while (*str)
-		str++, i++;
-	return (i);
-}
 /**
- * append_text_to_file - appends text to file
- * @filename: file's name
- * @text_content: text to be appended
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
  *
- * Return: 1 on success, -1 on failure
- *
-*/
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_WRONLY | O_APPEND);
-	ssize_t bytes = 0, len = _strlen(text_content);
+	int o, w, len = 0;
 
-	if (!filename || fd == -1)
+	if (filename == NULL)
 		return (-1);
-	if (len)
-		bytes = write(fd, text_content, len);
 
-	close(fd);
-	if (bytes == len)
-		return (1);
-	return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
